1a66e99e8035cbe839c97db1d67e6490daeaed94
Mamoon Bhuiyan <mamoon_131@hotmail.com>
2018-03-07 16:16:31 -0700

    hi

---
+++ project-20180307T211910Z-001/project/WpfApp1/WpfApp1/MainWindow.xaml.vb
@@ -0,0 +1,5 @@
+﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        connectionString = "Data Source=MAMOON-XPS;Initial Catalog=project;Integrated Security=True"
+    End Sub
+End Class
---
+++ project-20180307T211910Z-001/project/WpfApp1/WpfApp1/projectDataSet.vb
@@ -0,0 +1,4 @@
+﻿Partial Class projectDataSet
+    Partial Public Class MovieDataTable
+    End Class
+End Class
