61632b941f1e9c495c47c9445895470351219881
Adrian Cate <catea@mymacewan.ca>
2018-03-14 15:08:46 -0600

    V3

---
+++ Project_V3/Project_V3/Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lbl_Conn = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(304, 151)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lbl_Conn
+        '
+        Me.lbl_Conn.AutoSize = True
+        Me.lbl_Conn.Location = New System.Drawing.Point(106, 83)
+        Me.lbl_Conn.Name = "lbl_Conn"
+        Me.lbl_Conn.Size = New System.Drawing.Size(57, 20)
+        Me.lbl_Conn.TabIndex = 2
+        Me.lbl_Conn.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lbl_Conn)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lbl_Conn As Label
+End Class
---
+++ Project_V3/Project_V3/Form1.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+    Dim connection As New SqlConnection("Server = DESKTOP-4GN0VE3;Database = project;Integrated Security = true ")
+    Private Sub ContextMenuStrip1_Opening(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles ContextMenuStrip1.Opening
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If connection.State = ConnectionState.Closed Then
+            connection.Open()
+            'This will print a the label "Connected in a greeen color
+            lbl_Conn.Text = "Connected"
+            lbl_Conn.ForeColor = Color.Green
+        End If
+    End Sub
+
+    Private Sub lbl_Conn_Click(sender As Object, e As EventArgs) Handles lbl_Conn.Click
+
+    End Sub
+End Class
