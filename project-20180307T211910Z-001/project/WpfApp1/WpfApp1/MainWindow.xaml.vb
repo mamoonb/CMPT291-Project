9ecb52d965b446fa92514ef407291eed6c5157f4
Adrian Cate <catea@mymacewan.ca>
2018-03-19 16:11:18 -0600

    as

--- project-20180307T211910Z-001/project/WpfApp1/WpfApp1/MainWindow.xaml.vb
+++
@@ -1,9 +0,0 @@
-﻿Class MainWindow
-    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
-    End Sub
-
-    Private Shared Function GetConnectionString() As String
-        Return "Data Source=MAMOON-XPS;Initial Catalog=project;Integrated Security=True"
-
-    End Function
-End Class
