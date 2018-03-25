f39e93d7ec380a2052007c6557de0365f1d27b22
tahmid <tahmid808@gmail.com>
2018-03-24 23:25:27 -0600

    retrieving customer data done

--- Project_V3/Project_V3/My Project/Application.Designer.vb
+++ Project_V3/Project_V3/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Project_V3.Login
+            Me.MainForm = Global.Project_V3.EmployeeMenu
         End Sub
     End Class
 End Namespace
